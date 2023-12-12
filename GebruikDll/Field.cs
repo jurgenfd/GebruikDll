fe4ab857c4ddd8251601331fdce512b2f7d25fa4
Jurgen Doreleijers <jurgenfd@gmail.com>
2023-12-12 12:10:44 +0100

    Add project files.

---
+++ GebruikDll/Field.cs
@@ -0,0 +1,25 @@
+﻿using CODE_TempleOfDoom_DownloadableContent;
+using System.Reflection.Metadata.Ecma335;
+
+namespace GebruikDll
+{
+    internal class Field : IField
+    {
+        int v1 = 0;
+        public Field(int v1, int v2)
+        {
+        }
+
+        public bool CanEnter => true;
+
+        public IPlacable Item {
+            get => null;
+            set => v1 = 0;
+             }
+
+        public IField GetNeighbour(int direction)
+        {
+            return null;
+        }
+    }
+}
---
+++ GebruikDll/Program.cs
@@ -0,0 +1,52 @@
+﻿using CODE_TempleOfDoom_DownloadableContent;
+using System;
+
+namespace GebruikDll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Console.ReadLine();
+        }
+
+        Enemy enemy;
+        public Program()
+        {
+            enemy = new HorizontallyMovingEnemy(5, 1, 1, 1, 10)
+            {
+                // Subscribe(this); // JFD: enemy is niet langer observable zoals in Martijn's screencast
+                CurrentField = new Field(1, 1) // Verplicht te initialiseren
+            };
+
+            enemy.OnDeath += ReportDeath;
+
+            for (int i = 0; i < 10; i++)
+            {
+                enemy.Move();
+                showEnemy();
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                enemy.DoDamage(1); // JFD GetHurt in Martijn's screencast
+                showEnemy();
+                if (enemy.NumberOfLives <= 0)
+                {
+                    break;
+                }
+            }
+        }
+
+        private void ReportDeath(object? sender, EventArgs e)
+        {
+            Console.WriteLine($"enemy KILLED at: {enemy.CurrentXLocation} Y: {enemy.CurrentYLocation}");
+        }
+
+        private void showEnemy()
+        {
+            Console.WriteLine($"X: {enemy.CurrentXLocation} Y: {enemy.CurrentYLocation} lives: {enemy.NumberOfLives} ");
+        }
+    }
+}
